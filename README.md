a01ca378b27dc3269bc79c8b82da7fcd87dcd078
SonByeongJoo <thsqudwn05@gmail.com>
2022-12-27 23:01:34 +0900

    221227 access token과 refresh token을 어떻게 처리해야할지에 파일 작성 및 고민

--- README.md
+++ README.md
@@ -19,3 +19,24 @@
   
 4. 참고 레퍼런스
 - [Sequelize](https://sequelize.org/)
+  
+<br/>
+
+**221227** 
+<br/>
+1. 개발 내용
+- Sequelize를 통해 users 테이블과 JWT Token을 관리할 tokens 테이블 관계 매핑
+- util 디렉토리 내 jwt 유틸 함수 작성
+  
+2. 개발 이슈/고민
+- JWT를 이용해 access token과 refresh token을 쿠키에 삽입하여 관리할 수 있는 방법이 뭐가 있을지 고민이 된다.
+- 쿠키에 access token과 userid를 key-value 형식으로 담으면 보안에 문제 생기지 않을까 의심스럽다.
+- DB에 저장될 refresh token을 적재적소에 꺼내와야 트래픽 문제가 생기지 않을거라고 보는데, 어떻게 구현해야 될지 모르겠다.
+  
+3. 깨달은 점
+- module.expert 를 Array function 형식이 아닌 중괄호를 이용하여 '함수명:{사용할 내용}' 처럼 만들어준다면 가독성이 좋아지는 듯 하다.
+- expert와 module.expert는 다른 듯 하다.
+
+4. 참고 레퍼런스
+- [Express에서 JWT로 인증시스템 구현하기](https://velog.io/@kshired/Express%EC%97%90%EC%84%9C-JWT%EB%A1%9C-%EC%9D%B8%EC%A6%9D%EC%8B%9C%EC%8A%A4%ED%85%9C-%EA%B5%AC%ED%98%84%ED%95%98%EA%B8%B0-Access-Token%EA%B3%BC-Refresh-Token)
+- [안전하게 로그인 처리하기](https://overcome-the-limits.tistory.com/611)
--- SARAMARA_IN_TEST/server/util/jwt.js
+++ SARAMARA_IN_TEST/server/util/jwt.js
@@ -9,8 +9,44 @@ module.exports = {
             role : user.role
         };
 
-        return jwt.sign(payload, jwtsecret, {
-            algorithm: 'HS256'
+        return jwt.sign(payload, jwtsecret, { // jwtsecret으로 sign하여 발급 후 return
+            algorithm: 'HS256', // 암호화 알고리즘
+            expiresIn : '1h' // 유효기간
         })
+    },
+    verify: (token) => { // access token 검증
+        let decoded = null;
+        try{
+            decoded = jwt.verify(token, jwtsecret);
+            return {
+                ok: true,
+                id: decoded.id,
+                role : decoded.role,
+            };
+        } catch (err) {
+            return{
+                ok:false,
+                message : err.message,
+            };
+        }
+    },
+    refresh: () => { // refresh token 발급
+        return jwt.sign({}, jwtsecret, { // refresh token은 payload 없이 발급
+            algorithm: 'HS256',
+            expiresIn: '14d'
+        });
+    },
+    refreshVerify: async (token, userId) => { // refresh token 검증
+        /*
+        이 안에 tokens 테이블의 userid가 필요함.
+        */
+
+        try {
+            //refresh 토큰 가져올 때 필요.
+            // verify를 통해 토큰 검증
+        } catch(err) {
+            return false;
+        }
     }
-}
+
+};
